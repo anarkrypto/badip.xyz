6fa71cecfce8feefe7972f44c96039a854dc476d
anarkrypto <anarkrypto@gmail.com>
2025-06-08 03:42:41 -0300

    fix(api): return success: false in errorHandler

--- src/middlewares.ts
+++ src/middlewares.ts
@@ -2,7 +2,7 @@ import { Context } from 'hono'
 
 export const errorHandler = (err: Error, c: Context) => {
 	if (err instanceof Error) {
-		return c.json({ success: false, error: err.message }, 500)
+		return c.json({ success: false, error: err.message }, 500);
 	}
-	return c.json({ error: 'Unknown error' }, 500)
-}
+	return c.json({ success: false, error: "Unknown error" }, 500);
+};
